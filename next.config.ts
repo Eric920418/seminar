273bb62cefb31a60b1b28b12c060f45a0e86e20a
eric <26416387.re@gmail.com>
2025-03-25 18:13:49 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,25 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false, // 關閉 React Strict Mode
+  // 添加 CORS 設定
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config: Configuration, { isServer }: { isServer: boolean }) => {
     // 確保 config.resolve 與 alias 存在
     config.resolve = config.resolve || {};
